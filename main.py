49587926c300c65c6568839b82f19c24c12b8af2
Tae Lee <taebumlee@gmail.com>
2020-05-29 03:20:32 +0000

    list, dict, tuple

--- main.py
+++ main.py
@@ -71,5 +71,51 @@ while True:
   if i > 3:
     break
 
+# x = list()와 x = []는 동일하게 작동하는 리스트 정의
+# 하지만 x = list(1,2,3)은 안되고 list([1,2,3])으로 써야 한다.
+x = list([4,5,3])
+y = ["hello"]
+
+print(x + y)
+print(x[0])
+x[2] = 10
+print(x)
+
+num_elements = len(x)
+print(num_elements)
+
+for n in x:
+  print(n)
+
+print(y.index("hello"))
+print("bye" in y)
+
+if "hello" in y:
+  print("Hello is in list")
+
+# x = tuple() 또는 x = () 로 정의
+# 리스트에서 사용하는 거의 모든 함수나 메쏘드가 튜플에도 적용
+# 튜플은 assignment를 할 수 없다 
+# 튜플은 x[0] = 10 으로 할 수 없는 immutable 자료형이다. 
+
+# x = dict() 또는 x = {} 으로 정의
+# 딕셔너리는 key:value pair로 element를 구성한다. 
+
+x = {
+  "name":"Han",
+  "age":20,
+}
+
+print(x["name"], x["age"])
+print("age" in x)
+print(x.keys(), x.values())
+
+for key in x:
+  print(key, x[key])
+  print("key:" + str(key))
+  print("value:" + str(x[key]))
+
+x[0] = "Jay"
+print(x)  
 
 
